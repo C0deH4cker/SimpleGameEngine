cd330343e530ea0b7ce1d745c646d90ecbdbf21d
joshhuelsman <joshuahuelsman@yahoo.com>
2014-01-21 13:30:06 -0500

    Added skeleton struct for 4x4 matrices.

---
+++ Matrix4.h
@@ -0,0 +1,34 @@
+//
+//  Matrix4.h
+//  SimpleGameEngine
+//
+//  Created by Josh Huelsman on 1/20/14.
+//  Copyright (c) 2014 Josh Huelsman <joshuahuelsman@gmail.com>. All rights reserved.
+//
+
+#ifndef MATRIX4_H
+#define MATRIX4_H
+
+#include <stdint.h>
+
+namespace sge {
+	struct Matrix4
+	{
+		float m00, m01, m02, m03;
+		float m10, m11, m12, m13;
+		float m20, m21, m22, m23;
+		float m30, m31, m32, m33;
+
+		Matrix4();
+		~Matrix4();
+
+		float& operator[](uint8_t);
+
+		///@return a new 4x4 identity matrix.
+		static Matrix4& identity();
+	};
+}
+
+
+
+#endif
